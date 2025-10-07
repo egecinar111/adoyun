d82201e083f27fb2e34ec827060d06f2fae11bda
egecinar111 <80387534+egecinar111@users.noreply.github.com>
2025-10-07 11:08:25 +0300

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yourname.flappyads',
+  appName: 'Flappy Ads',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
